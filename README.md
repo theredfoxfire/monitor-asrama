0cb6ab9ec35138722e3b8abfeb33a81880f0be85
Hasan Setiawan <vizzlearn@gmail.com>
2015-10-31 08:43:52 +0700

    dinamic index

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 asrama
 ======
-
+Uname: admin
+Upass: admin or adminst3k1p
 A Symfony project created on September 29, 2015, 8:46 am.
--- index.php
+++ index.php
@@ -1,2 +1,4 @@
 <?php
-header('Location: http://www.asrama.stkipsurya.ac.id/web/app.php');
+$actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+header('Location: '.$actual_link.'web/app.php');
+exit();
